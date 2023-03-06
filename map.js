d956ccd4d8ad5d00fd12347d4ed07137a30bd7d4
Syrym Zhakypbekov <43728607+Syrym-Zhakypbekov@users.noreply.github.com>
2023-03-06 13:55:25 +0600

    test

--- map.js
+++ map.js
@@ -2,4 +2,5 @@
 setTimeout(() => {
     window.onload = document.body.querySelector('div.container-fluid.aa-footer').remove()
     window.onload = document.body.querySelector('div.container-fluid.aa-copyright').remove()
-}
+    }
+)
