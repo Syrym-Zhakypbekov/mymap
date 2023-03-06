5254494bc3258c78584ffc530e6b80130c7161ef
Syrym Zhakypbekov <43728607+Syrym-Zhakypbekov@users.noreply.github.com>
2023-03-06 13:30:51 +0600

    test

--- map.js
+++ map.js
@@ -2,9 +2,10 @@ console.log('test')
 window.onload = document.body.querySelector('div.container-fluid.aa-footer').remove()
 window.onload = document.body.querySelector('div.container-fluid.aa-copyright').remove()
 
-window.onload = function test(){
+function test(){
     document.body.querySelector('div.container-fluid.aa-footer').remove()
     document.body.querySelector('div.container-fluid.aa-copyright').remove()
 }
 
+window.onload =  test()
 test()
