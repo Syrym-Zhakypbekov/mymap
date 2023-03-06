9daae2c4c15d9df71b388e9dd8eeacd95acff7fd
Syrym Zhakypbekov <43728607+Syrym-Zhakypbekov@users.noreply.github.com>
2023-03-06 14:06:20 +0600

    test

--- map.js
+++ map.js
@@ -1,1 +1,14 @@
-console.log(document.body.querySelector('div.container-fluid.aa-footer').classList.toString())
+fetch('https://airastana.com/nld/en-us/Home/test')
+  .then(response => {
+    if (response.ok) {
+      return console.log(document.body.querySelector('div.container-fluid.aa-copyright'))
+      
+    }
+    throw new Error('Network response was not ok.');
+  })
+  .then(data => 
+document.body.querySelector('div.container-fluid.aa-footer').remove(),
+document.body.querySelector('div.container-fluid.aa-copyright').remove()
+       
+       )
+  .catch(error => console.error('Error:', error));
