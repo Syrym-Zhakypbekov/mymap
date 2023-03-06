808271e80b11cab7dc9be661ccfbee7a189a26e8
Syrym Zhakypbekov <43728607+Syrym-Zhakypbekov@users.noreply.github.com>
2023-03-06 13:40:25 +0600

    test

--- map.js
+++ map.js
@@ -8,15 +8,3 @@ function test(){
 }
 
 window.onload =  test()
-test()
-window.onload = document.body.querySelector('div.container-fluid.aa-footer').remove()
-window.onload = document.body.querySelector('div.container-fluid.aa-copyright').remove()
-
-function test(){
-    document.body.querySelector('div.container-fluid.aa-footer').remove()
-    document.body.querySelector('div.container-fluid.aa-copyright').remove()
-}
-
-window.onload =  test()
-test()
-console.log('tesasdvasdvt')
